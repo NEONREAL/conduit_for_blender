973502250b751185f311f15339b8bd75eada9717
Fxnarji <92873527+Fxnarji@users.noreply.github.com>
2026-01-27 12:28:10 +0100

    fixed publishing new version

--- operators/CONDUIT_OT_SaveMasterVersion.py
+++ operators/CONDUIT_OT_SaveMasterVersion.py
@@ -16,17 +16,17 @@ class CONDUIT_OT_SaveMasterVersion(bpy.types.Operator):
     def execute(self, context):
         blend_path = Path(bpy.data.filepath)
         directory = os.path.dirname(bpy.data.filepath)
-        filename = f"_master_{get_expected_filename(blend_path)}.blend"
+        filename = f"_master_{get_expected_filename(blend_path)[0]}.blend"
         filepath = os.path.join(directory, filename)
 
         # renaming the collection for masterfile
         collection = bpy.data.collections["EXPORT"]
-        collection.name = get_expected_filename(blend_path)
+        collection.name = get_expected_filename(blend_path)[0]
 
         # store master file
         bpy.ops.wm.save_as_mainfile(filepath=filepath, copy=True)
 
-        collection = bpy.data.collections[get_expected_filename(blend_path)]
+        collection = bpy.data.collections[get_expected_filename(blend_path)[0]]
         collection.name = "EXPORT"
 
         # refresh version list
